f7f7322713d3f91e3eecc97e5bac44d7ed541c17
Katie <katherinegnelson@lewisu.edu>
2022-04-11 15:33:50 -0500

    Added hw10 folder

---
+++ hw10/BusinessLoan.java
@@ -0,0 +1,35 @@
+/* This file contains a subclass of the abstract superclass Loan that represents information about a business loan.
+ * Assignment: 10
+ * Name: Katherine Nelson
+ * Date: 3/27/2022
+ * Professor: Sheikh Shamsuddin
+ * Class Code: CPSC-24500-002
+ * File Name: BusinessLoan.java
+ */
+
+package homework10;
+
+public class BusinessLoan extends Loan {
+
+	public BusinessLoan() {
+		super();
+	}
+
+	public BusinessLoan(String loanNumber, String customerLastName, double loanAmount, String term, double currentPrimeInterestRate) {
+		super(loanNumber, customerLastName, loanAmount, term);
+		
+		interestRate = currentPrimeInterestRate + 1;
+	}
+	
+	@Override
+	public String toString() {
+		return "Company Name: " + companyName + "\n"
+				+ "Loan Type: Business Loan" + "\n"
+				+ "Loan Number: " + loanNumber + "\n"
+				+ "Customer Last Name: " + customerLastName + "\n"
+				+ "Loan Amount: " + "$" + String.format("%.2f", loanAmount) + "\n"
+				+ "Interest Rate: " + interestRate + "%" + "\n"
+				+ "Term: " + term;
+	}
+	
+}
